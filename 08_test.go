5ab4efea0a17261c4211fc927bbdbf5dc9f031dd
Ivan Cheremisin <fess932@gmail.com>
2022-05-30 10:20:22 +0300

    8

---
+++ 08_test.go
@@ -0,0 +1,51 @@
+package l1_test
+
+import (
+	"strconv"
+	"testing"
+)
+
+func setBit(n int, idx uint, val bool) int {
+	// прибавляем или вычитаем степень двойки
+	if val {
+		return n + 1<<idx
+	}
+
+	return n - 1<<idx
+}
+
+func Test_number(t *testing.T) {
+	tcs := []struct {
+		idx         uint
+		n, expected int
+		value       bool
+	}{
+		{
+			n:        8,
+			idx:      0,
+			value:    true,
+			expected: 9,
+		},
+		{
+			n:        8,
+			idx:      1,
+			value:    true,
+			expected: 10,
+		},
+		{
+			n:        9,
+			idx:      3,
+			value:    false,
+			expected: 1,
+		},
+	}
+
+	for i, tc := range tcs {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			actual := setBit(tc.n, tc.idx, tc.value)
+			if actual != tc.expected {
+				t.Errorf("expected %d, got %d", tc.expected, actual)
+			}
+		})
+	}
+}
